01fcea0e2e0aca73b06f840258e6d3fcf72d1f27
snowschooled <qv9182ia@go.minneapolis.edu>
2020-03-20 10:25:49 -0500

    begin 1

---
+++ Q1.cpp
@@ -0,0 +1,54 @@
+//CSCI1730-Group5-assignment 3 problem 1
+//Joel Piontek, Andrew Amakye Ansah, Adem Boru
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+
+class complex {
+public:
+complex(){
+	a = 0;
+	b = 0;
+
+}
+double input(){
+	cout << "Enter a complex number a+bi: ";
+	cin >> a >> b >> i;
+	cout << endl;
+}
+double addition(){
+}
+double subtracttion(){
+}
+double multiplication(){
+}
+double division(){
+}
+double output(){
+}
+double convert(){
+}
+bool checkEqual(){
+}
+private:
+double a, b;
+char i;
+};
+
+
+int main(){
+
+
+	cout << "Select an option - (1) perform complex number arithmetic" <<endl;
+	cout << "                   (2) check for quadratic equation solution" << endl;
+	cout << "                   (3) exit" << endl;
+
+
+	complex userComplex;
+	userComplex.input();
+	return 0;
+}
